454de498952aec3e274abf346c72b0645abe13eb
Devpatel1103 <157067557+Devpatel1103@users.noreply.github.com>
2024-09-27 11:45:40 -0400

    Added function

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,7 +21,12 @@ namespace Assignment_1
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
+
     {
+
+        int dayCounter = 0;
+        double totalDays = 0;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -33,8 +38,17 @@ namespace Assignment_1
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void resetBtn_Click(object sender, RoutedEventArgs e)
+
         {
-            //messagesTextBox = string.Empty;
+
+            // Reset all values to Zero
+            messagesTextBox.Text = string.Empty;
+            dayLbl.Content = "Day 1";
+            displayMessagesTextBox.Text = string.Empty;
+
+            dailyAverageMessagesTextBox.Text = string.Empty;
+
+
 
             messagesTextBox.Focus();
 
@@ -46,9 +60,70 @@ namespace Assignment_1
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void enterBtn_Click(object sender, RoutedEventArgs e)
-        {
 
+        {
+            const int minValue = 0;
+            const int maxValue = 10000;
+            const int maxNumberOfDays = 7;
+
+
+            //
+            string userInput = messagesTextBox.Text;
+
+            //check blkanks 
+            if (isInputNotBlank(userInput)) 
+            {
+
+                if (isValidDouble(userInput)) {
+                    double userInputDouble = Convert.ToDouble(userInput);
+
+
+                    if (isValidRange(minValue, maxValue, userInputDouble)) {
+
+                        if (hasTxtBoxAnyValues(displayMessagesTextBox)) {
+                            displayMessagesTextBox.Text += Environment.NewLine + userInputDouble.ToString();
+                            
+                        }
+                        
+                        else {
+
+                            displayMessagesTextBox.Text = userInputDouble.ToString();
+                        }
+
+                        if (!isCrossMaxDays(maxNumberOfDays, dayCounter))
+                        {
+                            dayCounter += 1;
+                            totalDays += userInputDouble;
+                            dayLbl.Content = "Day " + (dayCounter + 1).ToString();
+                        }
+                        else { 
+                        
+                            enterBtn.IsEnabled = false;
+                            double countResult = averageGradeds(maxNumberOfDays, totalDays);
+                            dailyAverageMessagesTextBox.Text = countResult.ToString();
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("INPUT IS NOT WITHING RANGE");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Input must be double");
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("input should not be null ");
+            }
+
+            messagesTextBox.Text = string.Empty;
+            messagesTextBox.Focus();
         }
+
         
         /// <summary>
         /// This will exit the program
@@ -59,5 +134,74 @@ namespace Assignment_1
         {
             Application.Current.MainWindow.Close();
         }
+
+        /// <summary>
+        /// This function will
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private bool isInputNotBlank(string input) { 
+        
+            if (string.IsNullOrEmpty(input)) { return false ; }
+
+            return true;
+        }
+
+        /// <summary>
+        /// This function
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private bool isValidDouble(string input)
+        {
+
+            if (double.TryParse(input, out double result)) { return true; }
+            return false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="minValue"></param>
+        /// <param name="maxValue"></param>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private bool isValidRange(int minValue, int maxValue, double input) {
+
+            if (input >= minValue && input <= maxValue) {
+
+                return true; }
+
+            return false;
+        
+        
+        }
+
+        private bool hasTxtBoxAnyValues(TextBox displayMessagesTextBox)
+        {
+            if (!string.IsNullOrEmpty(displayMessagesTextBox.Text)) {
+
+                return true;
+            }
+
+            return false;
+
+        }
+
+        private bool isCrossMaxDays(int maxNumberOfDays, int dayNumber) {
+
+            if (dayNumber + 1 >= maxNumberOfDays) { 
+                return true;
+            }
+
+            return false ;
+        }
+
+
+        private double averageGradeds(int numberOfDays, double totalDays) { 
+        
+            return totalDays / numberOfDays;
+        }
     }
+
 }
